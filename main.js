bbf13279527b888a9bd3dec09314f9fd18608cdf
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-07 00:26:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
+const strike = document.querySelectorAll(".ol")
+for(let i = 0;i < strike.length; i ++){
+    strike[i].addEventListener("click",()=>{
+        strike[i].style.textDecoration = "line-through"
+    })
+}
+const opaque = document.querySelectorAll(".ul")
+for(let i = 0; i < opaque.length; i++){
+    opaque[i].addEventListener("click",()=>{
+        opaque[i].style.opacity = ".25"
+    })    
+}
 
+const picture = document.querySelectorAll(".pic")
+
+for(let i = 0; i < picture.length; i++){
+    picture[i].addEventListener("click",()=>{
+        picture[i].style.opacity = ".5"
+    })    
+}
+document.querySelector("#destroy-all").addEventListener("click",()=>{
+    for(let i = 0; i < strike.length; i++){
+        strike[i].style.textDecoration = "line-through"
+    }
+    for(let i = 0;i < opaque.length;i++){
+        opaque[i].style.opacity = ".25"
+    }
+    for(let i = 0;i < picture.length; i++){
+        picture[i].style.opacity = ".5"
+    }
+})
